6c3d8c74bc43792fa9bfc641c16c466f3a709cde
brnydr <brianwilliamyoder@gmail.com>
2023-07-14 21:57:40 -0700

    add totalCost to order object, refactor

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@ class Order {
     constructor() {
         this.pizzas = [];
         this.currentId = 0;
+        this.totalCost = 0;
     }
     addPizza(pizza) {
         pizza.id = this.assignId();
@@ -35,31 +36,70 @@ function getToppings() {
 }
 //not adding toppings
 function addPizzaToOrder() {
-    
     let size = document.getElementById("size").value;
     let toppings = getToppings();
     let pizza = new Pizza(size, toppings);
+    price = determinePrice(pizza);
+    pizza.price = price;
+    order.totalCost += price;
     order.addPizza(pizza);
     displayPizzas(pizza)
+    size.value = "xl";
+    resetCheckboxes();
 }
 
-
+function resetCheckboxes() {
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+    checkboxes.forEach((checkbox) => {
+      checkbox.checked = false;
+    });
+  }
+  
+function determinePrice(pizza) {
+    let price = 0;
+    if (pizza.size === "xl") {
+        price += 16;
+    } else if (pizza.size === "large") {
+        price += 15;
+    } else if (pizza.size === "medium") {
+        price += 13;
+    } else if (pizza.size === "small") {
+        price += 10;
+    }
+    pizza.toppings.forEach(function(topping) {
+        price += 1;
+    });
+    return price;
+}
 
 function displayPizzas(pizza) {
     let li = document.createElement("li");
     let pizzaList = document.getElementById("pizzaList");
+    let pizzaString = "";
     pizzaList.append(li);
     li.innerText
-    let pizzaString = pizza.size + " with " + pizza.toppings;
+    if (pizza.toppings.length === 0) {
+        pizzaString = pizza.size + " with just cheese" + " $" + pizza.price;
+    } else {
+        pizzaString = pizza.size + " with " + pizza.toppings + " $" + pizza.price;
+    };
     li.innerText = pizzaString;
     document.querySelector("#orderSummary").classList.remove("hidden");
 }
 
-// function finalizeOrder(e) {
-// e.preventDefault();
-// }
+function finalizeOrder(e) {
+e.preventDefault();
+let orderTotal = document.getElementById("orderTotal");
+let orderMessage = document.getElementById("orderMessage");
+
+let orderDiv = document.getElementById("orderDiv");
+orderMessage.innerText = "Your total is $" + order.totalCost;
+orderDiv.classList.add("hidden");
+orderTotal.innerText = "Your total is $" + order.totalCost;
+
+}
 
 window.addEventListener("load", function() {
     document.getElementById("addPizza").addEventListener("click", addPizzaToOrder);
-    // document.querySelector("form").addEventListener("submit", finalizeOrder());
+    document.querySelector("form").addEventListener("submit", finalizeOrder);
 });
